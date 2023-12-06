0c53bc9a0cefe6f370e8a6c0198ad2b7d4e9f8fe
IsabelTizon <tizonarranz@gmail.com>
2023-12-06 15:40:51 +0000

    fixing errors

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,10 +1,10 @@
 import Link from "next/link";
-export default function About() {
+export default function Contact() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
 			<div>
-				<h2 className="text-black">ABOUT ME</h2>
-				<Link href="/">Go back home</Link>
+				<h2 className="text-black">CONTACT ME</h2>
+				<Link href="/">Go back to home</Link>
 			</div>
 		</main>
 	);
