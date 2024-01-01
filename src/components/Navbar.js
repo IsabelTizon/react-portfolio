168717c2c10cc63b057f3d520802e033a119bc55
IsabelTizon <tizonarranz@gmail.com>
2024-01-01 16:31:07 +0100

    React icons package installed
    npm install react-icons --save

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,16 @@
 "use client";
 import Link from "next/link";
+import { useState } from "react";
+import {
+	AiOutlineMenu,
+	AiOutlineClose,
+} from "react-icons/ai";
 
 function Navbar() {
+	const [menuIcon, setIcon] = useState(false);
+	const handleSmallerScreemNavigation = () => {
+		setIcon(!menuIcon);
+	};
 	return (
 		<header className="bg-slate-400 text-black w-full ease-in duration-300 fixed top-0 left-0 z-10">
 			<nav className="max-w-[1366px] mx-auto h-[80px] flex justify-between items-center  p-4">
@@ -13,8 +22,9 @@ function Navbar() {
 						Isabel Arranz
 					</Link>
 				</div>
-				{/*;a*/}
-				<ul className="hidden md:flex uppercase font-semibold text-xl lg:text-[15px] text-slate-800">
+
+				{/*larger screen navigation */}
+				<ul className="hidden md:flex uppercase font-semibold text-xl md:text-[14px] lg:text-[15px] text-slate-800">
 					<li className="mr-4 lg:mr-8 hover:text-[#381638]">
 						<Link href="/blog">Blog</Link>
 					</li>
@@ -24,7 +34,7 @@ function Navbar() {
 					<li className="mr-4 lg:mr-8 hover:text-[#381638]">
 						<Link href="/contact">contact</Link>
 					</li>
-					<li className="mr-4 lg:mr-8 text-[#f9f459] hover:text-black">
+					<li className="text-[#f9f459] hover:text-black">
 						<Link
 							target="_blank"
 							href="https://github.com/IsabelTizon"
@@ -33,6 +43,26 @@ function Navbar() {
 						</Link>
 					</li>
 				</ul>
+
+				{/*smaller screen navigation */}
+				<div
+					onClick={handleSmallerScreemNavigation}
+					className="flex md-hidden"
+				>
+					{menuIcon ? (
+						<AiOutlineClose
+							size={25}
+							className="text-[#CEFF00]"
+						/>
+					) : (
+						<AiOutlineMenu
+							size={25}
+							className="text-[#CEFF00]"
+						/>
+					)}
+				</div>
+
+				{/*smaller screens Icons */}
 			</nav>
 		</header>
 	);
