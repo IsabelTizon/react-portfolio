29d1c9941a0f79828bc77e6e4cf601ceff52c98a
IsabelTizon <tizonarranz@gmail.com>
2024-01-01 13:58:42 +0100

    Navigation styles
    navigation to another window:
    
    <Link target="_blank" href="https://github.com/IsabelTizon">Github</Link>

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,15 +3,36 @@ import Link from "next/link";
 
 function Navbar() {
 	return (
-		<header className="bg-slate-400 text-[#CEFF00] w-full ease-in duration-300 fixed top-0 left-0 z-10">
+		<header className="bg-slate-400 text-black w-full ease-in duration-300 fixed top-0 left-0 z-10">
 			<nav className="max-w-[1366px] mx-auto h-[80px] flex justify-between items-center  p-4">
-				<div>
-					<Link href="/">About</Link>
-					<Link href="/blog">Blog</Link>
-					<Link href="/projects">projects</Link>
-					<Link href="/contact">contact me</Link>
-					<Link href="/contact">Github</Link>
+				<div className="">
+					<Link
+						className="uppercase font-extrabold text-3xl md:text-2xl lg:text-3xl text-slate-800 hover:text-[#381638]"
+						href="/"
+					>
+						Isabel Arranz
+					</Link>
 				</div>
+				{/*;a*/}
+				<ul className="hidden md:flex uppercase font-semibold text-xl lg:text-[15px] text-slate-800">
+					<li className="mr-4 lg:mr-8 hover:text-[#381638]">
+						<Link href="/blog">Blog</Link>
+					</li>
+					<li className="mr-4 lg:mr-8 hover:text-[#381638]">
+						<Link href="/projects">projects</Link>
+					</li>
+					<li className="mr-4 lg:mr-8 hover:text-[#381638]">
+						<Link href="/contact">contact</Link>
+					</li>
+					<li className="mr-4 lg:mr-8 text-[#f9f459] hover:text-black">
+						<Link
+							target="_blank"
+							href="https://github.com/IsabelTizon"
+						>
+							Github
+						</Link>
+					</li>
+				</ul>
 			</nav>
 		</header>
 	);
