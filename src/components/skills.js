c4b199a94d87ed3496aa7f6d3e65368c8efd6fbd
IsabelTizon <tizonarranz@gmail.com>
2024-01-25 15:24:06 +0000

    array skills

---
+++ src/components/skills.js
@@ -0,0 +1,74 @@
+export const myfrontendSkills = [
+	{
+		id: 0,
+		name: "HTML5 and CSS",
+	},
+	{
+		id: 1,
+		name: "JavaScript",
+	},
+	{
+		id: 2,
+		name: "jQuery",
+	},
+	{
+		id: 3,
+		name: "Bootstrap",
+	},
+	{
+		id: 4,
+		name: "AngularJs",
+	},
+	{
+		id: 5,
+		name: "React",
+	},
+	{
+		id: 6,
+		name: "MUI, Tailwind CSS",
+	},
+];
+export const mybackendSkills = [
+	{
+		id: 0,
+		name: "PHP",
+	},
+	{
+		id: 1,
+		name: "mySQL",
+	},
+];
+export const myOtherSkills = [
+	{
+		id: 0,
+		name: "UX",
+	},
+	{
+		id: 1,
+		name: "Figma",
+	},
+	{
+		id: 2,
+		name: "Illustrator",
+	},
+	{
+		id: 3,
+		name: "Photoshop",
+	},
+	{
+		id: 4,
+		name: "Project Manager Sftware",
+	},
+	{
+		id: 5,
+		name: "Jira",
+	},
+	{
+		id: 6,
+		name: "CMS",
+	},
+	{
+		id: 7,
+		name: "GitHub",
+	},
+];
