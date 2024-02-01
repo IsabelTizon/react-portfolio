ae419a940d7d5c2ca7e3842fd216ec4a4ec83fd8
IsabelTizon <tizonarranz@gmail.com>
2024-02-01 11:36:09 +0000

    card project section

--- src/app/page.js
+++ src/app/page.js
@@ -22,7 +22,7 @@ export default function About() {
 			{/** xl-[] LAPTOP => min-width: 1280px */}
 			{/** 2xl-[] SCREEN => min-width: 1536px */}
 
-			{/** -[]  md:text-[] lg:text-[] xl:text-[] 2xl:text-[] */}
+			{/** w-full  md:w-1/3 lg:text-[] xl:text-[] 2xl:text-[] */}
 
 			{/* hero */}
 			<div class="bg-gray-900  w-full p-10 md:p-72 lg:p-40 xl:p-[30rem] 2xl:p-[55rem]">
@@ -185,16 +185,202 @@ export default function About() {
 					</div>
 				</div>
 			</div>
+
 			{/* Project's sections */}
 			<div className="py-24 px-10 xl:px-[35rem] 2xl:px-[55rem] xl:py-[15rem] 2xl:py-[20rem]">
 				<h2 className="">projects section</h2>
+				<p className="pb-40">
+					Lorem Ipsum is simply dummy text of the printing
+					and typesetting industry. Lorem Ipsum has been the
+					Lorem Ipsum is simply dummy text of the printing
+					and typesetting industry. Lorem Ipsum has been the
+					industrys standard dummy text ever since the
+					1500s, when an unknown printer took a galley of
+					type and scrambled it to
+				</p>
+				<div className="flex flex-wrap gap-4">
+					<div class="w-full md:w-[32%] lg:w-[32%] xl:w-[32%] 2xl:w-[32%] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+						<Link href="/contact">
+							<Image
+								className="w-full h-[45%]"
+								width={300}
+								height={300}
+								src="/img/demoScreen.png"
+								alt="project picture"
+							/>
+						</Link>
+						<div class="p-5">
+							<h3 className="text-black font-bold uppercase pb-8">
+								frontend projects
+							</h3>
+							<p className="text-[3rem]">
+								Lorem Ipsum is simply dummy text of the
+								printing and typesetting industry. Lorem
+								Ipsum has been the Lorem Ipsum
+							</p>
+							<Link href="/projects">
+								<button className="mustard">
+									view portfolio
+								</button>
+							</Link>
+						</div>
+					</div>
+					<div class="w-full md:w-[32%] lg:w-[32%] xl:w-[32%] 2xl:w-[32%] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+						<a href="/contact">
+							<Image
+								width={300}
+								height={300}
+								src="/img/demoScreen.png"
+								alt="project picture"
+							/>
+						</a>
+						<div class="p-5">
+							<a href="#">
+								<h3 className="text-black font-bold uppercase pb-8">
+									frontend projects
+								</h3>
+							</a>
+							<p className="pb-40">
+								Lorem Ipsum is simply dummy text of the
+								printing and typesetting industry. Lorem
+								Ipsum has been the Lorem Ipsum
+							</p>
+							<a
+								href="#"
+								class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+							>
+								Read more
+								<svg
+									class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+									aria-hidden="true"
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 14 10"
+								>
+									<path
+										stroke="currentColor"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="2"
+										d="M1 5h12m0 0L9 1m4 4L9 9"
+									/>
+								</svg>
+							</a>
+						</div>
+					</div>
+					<div class="w-full md:w-[32%] lg:w-[32%] xl:w-[32%] 2xl:w-[32%] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+						<a href="/contact">
+							<Image
+								width={300}
+								height={300}
+								src="/img/demoScreen.png"
+								alt="project picture"
+							/>
+						</a>
+						<div class="p-5">
+							<a href="#">
+								<h3 className="text-black font-bold uppercase pb-8">
+									frontend projects
+								</h3>
+							</a>
+							<p className="pb-40">
+								Lorem Ipsum is simply dummy text of the
+								printing and typesetting industry. Lorem
+								Ipsum has been the Lorem Ipsum
+							</p>
+							<a
+								href="#"
+								class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+							>
+								Read more
+								<svg
+									class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+									aria-hidden="true"
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 14 10"
+								>
+									<path
+										stroke="currentColor"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="2"
+										d="M1 5h12m0 0L9 1m4 4L9 9"
+									/>
+								</svg>
+							</a>
+						</div>
+					</div>
+
+					{/* <div className="h-150 w-full object-containd">
+						<Link href={"/contact"}>
+							<Image
+								width={300}
+								height={300}
+								src="/img/demoScreen.png"
+								alt="project picture"
+							/>
+							<h3 className="text-black font-bold uppercase pb-8">
+								frontend projects
+							</h3>
+							<p className="pb-40">
+								Lorem Ipsum is simply dummy text of the
+								printing and typesetting industry. Lorem
+								Ipsum has been the Lorem Ipsum
+							</p>
+						</Link>
+					</div>
+					<div className="h-150 w-full object-containd">
+						<Link
+							href={"/contact"}
+							className="h-150 w-full object-containd"
+						>
+							<Image
+								width={300}
+								height={300}
+								src="/img/demoScreen.png"
+								alt="project picture"
+							/>
+							<h3 className="text-black font-bold uppercase pb-8">
+								backend projects
+							</h3>
+							<p className="pb-40">
+								Lorem Ipsum is simply dummy text of the
+								printing and typesetting industry. Lorem
+								Ipsum has been the Lorem Ipsum
+							</p>
+						</Link>
+					</div>
+					<div className="h-150 w-full object-containd">
+						<Link
+							href={"/contact"}
+							className="h-150 w-full object-containd"
+						>
+							<Image
+								width={300}
+								height={300}
+								src="/img/demoScreen.png"
+								alt="project picture"
+							/>
+							<h3 className="text-black font-bold uppercase pb-8">
+								team projects
+							</h3>
+							<p className="pb-40">
+								Lorem Ipsum is simply dummy text of the
+								printing and typesetting industry. Lorem
+								Ipsum has been the Lorem Ipsum
+							</p>
+						</Link>
+					</div> */}
+				</div>
 			</div>
+
 			{/* Are You Ready To Start New Project With Me? */}
 			<div className="bg-gray-900 ">
 				<div className="flex justify-center mx-[40px] md:mx-[150px] lg:mx-[300px]  xl:mx-[250px] 2xl:mx-[400px]">
 					<div className="mx-full py-24">
-						<div className="flex justify-center">
-							<h2 className="text-white">
+						<div className="flex justify-center xl:mx-[25rem]">
+							<h2 className="text-[#b38600] text-center">
 								are you ready to start a new project with
 								me?
 							</h2>
@@ -222,125 +408,5 @@ export default function About() {
 				</div>
 			</div>
 		</main>
-		// <main className="flex min-h-screen flex-col items-center justify-between p-24">
-		// 	<div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-		// 		<p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-		// 			Get started by editing&nbsp;
-		// 			<code className="font-mono font-bold">
-		// 				src/app/page.js
-		// 			</code>
-		// 		</p>
-		// 		<div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-		// 			<a
-		// 				className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-		// 				href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-		// 				target="_blank"
-		// 				rel="noopener noreferrer"
-		// 			>
-		// 				By{" "}
-		// 				<Image
-		// 					src="/vercel.svg"
-		// 					alt="Vercel Logo"
-		// 					className="dark:invert"
-		// 					width={100}
-		// 					height={24}
-		// 					priority
-		// 				/>
-		// 			</a>
-		// 		</div>
-		// 	</div>
-
-		// 	<div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-		// 		<Image
-		// 			className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-		// 			src="/next.svg"
-		// 			alt="Next.js Logo"
-		// 			width={180}
-		// 			height={37}
-		// 			priority
-		// 		/>
-		// 	</div>
-
-		// 	<div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-		// 		<a
-		// 			href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-		// 			className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-		// 			target="_blank"
-		// 			rel="noopener noreferrer"
-		// 		>
-		// 			<h2 className={`mb-3 text-2xl font-semibold`}>
-		// 				Docs{" "}
-		// 				<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-		// 					-&gt;
-		// 				</span>
-		// 			</h2>
-		// 			<p
-		// 				className={`m-0 max-w-[30ch] text-sm opacity-50`}
-		// 			>
-		// 				Find in-depth information about Next.js features
-		// 				and API.
-		// 			</p>
-		// 		</a>
-
-		// 		<a
-		// 			href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-		// 			className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-		// 			target="_blank"
-		// 			rel="noopener noreferrer"
-		// 		>
-		// 			<h2 className={`mb-3 text-2xl font-semibold`}>
-		// 				Learn{" "}
-		// 				<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-		// 					-&gt;
-		// 				</span>
-		// 			</h2>
-		// 			<p
-		// 				className={`m-0 max-w-[30ch] text-sm opacity-50`}
-		// 			>
-		// 				Learn about Next.js in an interactive course
-		// 				with&nbsp;quizzes!
-		// 			</p>
-		// 		</a>
-
-		// 		<a
-		// 			href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-		// 			className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-		// 			target="_blank"
-		// 			rel="noopener noreferrer"
-		// 		>
-		// 			<h2 className={`mb-3 text-2xl font-semibold`}>
-		// 				Templates{" "}
-		// 				<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-		// 					-&gt;
-		// 				</span>
-		// 			</h2>
-		// 			<p
-		// 				className={`m-0 max-w-[30ch] text-sm opacity-50`}
-		// 			>
-		// 				Explore starter templates for Next.js.
-		// 			</p>
-		// 		</a>
-
-		// 		<a
-		// 			href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-		// 			className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-		// 			target="_blank"
-		// 			rel="noopener noreferrer"
-		// 		>
-		// 			<h2 className={`mb-3 text-2xl font-semibold`}>
-		// 				Deploy{" "}
-		// 				<span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-		// 					-&gt;
-		// 				</span>
-		// 			</h2>
-		// 			<p
-		// 				className={`m-0 max-w-[30ch] text-sm opacity-50`}
-		// 			>
-		// 				Instantly deploy your Next.js site to a
-		// 				shareable URL with Vercel.
-		// 			</p>
-		// 		</a>
-		// 	</div>
-		// </main>
 	);
 }
