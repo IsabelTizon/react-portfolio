a936e06d149bf0d1ffbdbf962abe4a754d828a9f
IsabelTizon <tizonarranz@gmail.com>
2024-02-09 12:50:26 +0000

    array projects new orden

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,14 +1,72 @@
 export const myProjects = [
 	{
 		id: 0,
-		name: "Pass Recipe",
+		name: "BWM",
 		description: [
-			"Pass Recipe is a website created in the second block of the Web Development HND course at City of Glasgow College for the subject Graded Unit 2.",
+			`BWM was the first website I made, this took place in the first block of HNC in my Web Design and Development course at City of Glasgow College for the subject of Website Essential Content.`,
+			`The purpose of the BWM was to create a website with the purpose of transmitting musical content. The student chose the musical theme and the style of the page.`,
+		],
+		partsProject: [
+			`To carry out the project I divided it into four stages. The BWM´s first stage was the analysis stage, it was dedicated to understand the usage content. In it, I identified the purpose and objective of the website and detail the functional and non-functional requirements. Also, specify the target audience, creating user profiles and preparing a study of user interaction details.`,
+			`In the second stage I carried out the design step. In BWM I created some wireframes and storyboards in Figma as well as a sitemap with the same tool. It also required a style guide specifying the colours and fonts and the reason for them.`,
+			`Being the first website, BMW uses very simple technologies: HTML5 and CSS. I used a grid-based layout system to be able to order the elements and make the page responsive. You can view the page on desktop, tablet and mobile devices. BWM also required the use of photoshop and illustrator to create the logo. I decided to also create the festival posters since I added a part of the web titled festivals.`,
+			`The fourth part was based on testing the page. For this I used W3Validator for HTML and W3Validator for CSS and in Lighthouse to validate BWM. In addition to completing testing level a test plan documentation was required.`,
+			,
 		],
 		pic: "/img/demoScreen.png",
 	},
 	{
 		id: 1,
+		name: "Coulorie Counter",
+		description: [
+			`Calorie Counter is part of the work that you can find in my portfolio, it is an app created in the second block of the Web Development HNC course at City of Glasgow College for the subject Developing Mobile Web Based Applications: An Introduction. This Unit is designed to introduce learners to the design and production for web based apps for mobile devices.`,
+			`The web-based application is to promote a healthier lifestyle, to encourage people to be aware of their calorie intake and to control it accordingly.`,
+			`All people who want to count the calories they eat or who simply want to be aware of it. It will also be useful for people who want advice on healthy living, as the app, in addition to a calorie counter, will come with a healthy lifestyle section that will include workouts, recipes and meditation audios.`,
+		],
+		partsProject: [
+			`The entire project was based on four stages. The first stage, the analysis stage, was dedicated to understanding the usage content. In it, identify the purpose and objective of the website and detail the functional and non-functional requirements. Also specify the target audience, creating user profiles.`,
+			`The second stage of the project was based on the design. In it I created some wireframes and storyboards in Figma as well as a sitemap and a logic chart with the same tool. It also required a style guide specifying the colours and fonts and the reason for them.`,
+			`The development of the project occurred in its third stage. This app uses the following technologies: HTML5, CSS, JavaScript, along with Bootstrap, Font Awesome.`,
+			`The fourth part was based on testing the page. For this I used W3Validator for HTML and W3Validator for CSS and in Lighthouse. In addition to completing a test plan documentation.`,
+			,
+		],
+		pic: "/img/demoScreen.png",
+	},
+	{
+		id: 2,
+		name: "Winter Adventures",
+		description: [
+			`Winter Adventures is a website created in the second block of the HNC course at City of Glasgow College for the subject Web Technologies 1 – HTML & CSS and recovered to make some additions and improvements with new technologies in the first block of the HND course for the subject Web Technologies 2: HTML and JavaScript.`,
+			`The end of the Winter Adventures project was create a website with the purpose of to promote ski deals and trips to the snow.`,
+			`The web follows a hierarchical structure and is available for desktop, tablet and mobile.`,
+		],
+		partsProject: [
+			`The entire project was based on four stages. The first stage of the Winter Adventures was the analysis stage.In which one I looked into to understand the usage content. In it, identify the purpose and objective of the website and detail the functional and non-functional requirements. Also specify the target audience, creating user profiles and preparing a study of user interaction details.`,
+			`The second stage of the project was based on the design. In which one I created some wireframes and storyboards in Figma as well as a sitemap with the same tool. It also required a style guide specifying the colours and fonts and the reason for them in my Winter Adventures site.`,
+			`The development of the project occurred in its third stage. In that level I used the following technologies: HTML5, CSS and JavaScript, along with Bootstrap, AngularJS, Here API, Font Awesome.`,
+			`The fourth part was based on testing the page. For this I used W3Validator for HTML and W3Validator for CSS and in Lighthouse. In addition to completing the testing level I a test plan documentation.`,
+			,
+		],
+		pic: "/img/demoScreen.png",
+	},
+	{
+		id: 3,
+		name: "Get Media",
+		description: [
+			`GetMedia is a website created in the first block of the HND course at City of Glasgow College for the subject Design and Developing an Interactive Product. `,
+			`The end of the project was create a website with the purpose of streaming music content through providers such as Apple, it also wants to sell coupons initially only with iTunes although in the future it would like to open its business to different providers.`,
+		],
+		partsProject: [
+			`The entire project was based on four stages. The first stage, the analysis stage, was dedicated to understanding the usage content. In it, identify the purpose and objective of the website and detail the functional and non-functional requirements. Also specify the target audience, creating user profiles and preparing a study of user interaction details.`,
+			`The second stage of the project was based on the design. In it I created some wireframes and storyboards in Figma as well as a sitemap with the same tool. It also required a style guide specifying the colors and fonts and the reason for them.`,
+			`The development of the project occurred in its third stage. The page uses the following technologies: HTML5, CSS and JavaScript, along with Bootstrap, AngularJS, Here API, Font Awesome.`,
+			`The fourth part was based on testing the page. For this I used W3Validator for HTML and W3Validator for CSS and in Lighthouse. In addition to completing a test plan documentation. The fourth part was based on testing the page. For this, W3Validator for HTML and W3Validator for CSS and in Lighthouse were used.`,
+			,
+		],
+		pic: "/img/demoScreen.png",
+	},
+	{
+		id: 4,
 		name: "Mode",
 		description: [
 			`Mode is part of the work that you can find in my portfolio, it is a website created in the second block of the Web Development HND course at City of Glasgow College for the subject Design and development of an interactive product.`,
@@ -26,68 +84,10 @@ export const myProjects = [
 		pic: "/img/demoScreen.png",
 	},
 	{
-		id: 2,
-		name: "Get Media",
-		description: [
-			`GetMedia is a website created in the first block of the HND course at City of Glasgow College for the subject Design and Developing an Interactive Product. `,
-			`The end of the project was create a website with the purpose of streaming music content through providers such as Apple, it also wants to sell coupons initially only with iTunes although in the future it would like to open its business to different providers.`,
-		],
-		partsProject: [
-			`The entire project was based on four stages. The first stage, the analysis stage, was dedicated to understanding the usage content. In it, identify the purpose and objective of the website and detail the functional and non-functional requirements. Also specify the target audience, creating user profiles and preparing a study of user interaction details.`,
-			`The second stage of the project was based on the design. In it I created some wireframes and storyboards in Figma as well as a sitemap with the same tool. It also required a style guide specifying the colors and fonts and the reason for them.`,
-			`The development of the project occurred in its third stage. The page uses the following technologies: HTML5, CSS and JavaScript, along with Bootstrap, AngularJS, Here API, Font Awesome.`,
-			`The fourth part was based on testing the page. For this I used W3Validator for HTML and W3Validator for CSS and in Lighthouse. In addition to completing a test plan documentation. The fourth part was based on testing the page. For this, W3Validator for HTML and W3Validator for CSS and in Lighthouse were used.`,
-			,
-		],
-		pic: "/img/demoScreen.png",
-	},
-	{
-		id: 3,
-		name: "Winter Adventures",
-		description: [
-			`Winter Adventures is a website created in the second block of the HNC course at City of Glasgow College for the subject Web Technologies 1 – HTML & CSS and recovered to make some additions and improvements with new technologies in the first block of the HND course for the subject Web Technologies 2: HTML and JavaScript.`,
-			`The end of the Winter Adventures project was create a website with the purpose of to promote ski deals and trips to the snow.`,
-			`The web follows a hierarchical structure and is available for desktop, tablet and mobile.`,
-		],
-		partsProject: [
-			`The entire project was based on four stages. The first stage of the Winter Adventures was the analysis stage.In which one I looked into to understand the usage content. In it, identify the purpose and objective of the website and detail the functional and non-functional requirements. Also specify the target audience, creating user profiles and preparing a study of user interaction details.`,
-			`The second stage of the project was based on the design. In which one I created some wireframes and storyboards in Figma as well as a sitemap with the same tool. It also required a style guide specifying the colours and fonts and the reason for them in my Winter Adventures site.`,
-			`The development of the project occurred in its third stage. In that level I used the following technologies: HTML5, CSS and JavaScript, along with Bootstrap, AngularJS, Here API, Font Awesome.`,
-			`The fourth part was based on testing the page. For this I used W3Validator for HTML and W3Validator for CSS and in Lighthouse. In addition to completing the testing level I a test plan documentation.`,
-			,
-		],
-		pic: "/img/demoScreen.png",
-	},
-	{
-		id: 4,
-		name: "Coulorie Counter",
-		description: [
-			`Calorie Counter is part of the work that you can find in my portfolio, it is an app created in the second block of the Web Development HNC course at City of Glasgow College for the subject Developing Mobile Web Based Applications: An Introduction. This Unit is designed to introduce learners to the design and production for web based apps for mobile devices.`,
-			`The web-based application is to promote a healthier lifestyle, to encourage people to be aware of their calorie intake and to control it accordingly.`,
-			`All people who want to count the calories they eat or who simply want to be aware of it. It will also be useful for people who want advice on healthy living, as the app, in addition to a calorie counter, will come with a healthy lifestyle section that will include workouts, recipes and meditation audios.`,
-		],
-		partsProject: [
-			`The entire project was based on four stages. The first stage, the analysis stage, was dedicated to understanding the usage content. In it, identify the purpose and objective of the website and detail the functional and non-functional requirements. Also specify the target audience, creating user profiles.`,
-			`The second stage of the project was based on the design. In it I created some wireframes and storyboards in Figma as well as a sitemap and a logic chart with the same tool. It also required a style guide specifying the colours and fonts and the reason for them.`,
-			`The development of the project occurred in its third stage. This app uses the following technologies: HTML5, CSS, JavaScript, along with Bootstrap, Font Awesome.`,
-			`The fourth part was based on testing the page. For this I used W3Validator for HTML and W3Validator for CSS and in Lighthouse. In addition to completing a test plan documentation.`,
-			,
-		],
-		pic: "/img/demoScreen.png",
-	},
-	{
 		id: 5,
-		name: "BWM",
+		name: "Pass Recipe",
 		description: [
-			`BWM was the first website I made, this took place in the first block of HNC in my Web Design and Development course at City of Glasgow College for the subject of Website Essential Content.`,
-			`The purpose of the BWM was to create a website with the purpose of transmitting musical content. The student chose the musical theme and the style of the page.`,
-		],
-		partsProject: [
-			`To carry out the project I divided it into four stages. The BWM´s first stage was the analysis stage, it was dedicated to understand the usage content. In it, I identified the purpose and objective of the website and detail the functional and non-functional requirements. Also, specify the target audience, creating user profiles and preparing a study of user interaction details.`,
-			`In the second stage I carried out the design step. In BWM I created some wireframes and storyboards in Figma as well as a sitemap with the same tool. It also required a style guide specifying the colours and fonts and the reason for them.`,
-			`Being the first website, BMW uses very simple technologies: HTML5 and CSS. I used a grid-based layout system to be able to order the elements and make the page responsive. You can view the page on desktop, tablet and mobile devices. BWM also required the use of photoshop and illustrator to create the logo. I decided to also create the festival posters since I added a part of the web titled festivals.`,
-			`The fourth part was based on testing the page. For this I used W3Validator for HTML and W3Validator for CSS and in Lighthouse to validate BWM. In addition to completing testing level a test plan documentation was required.`,
-			,
+			"Pass Recipe is a website created in the second block of the Web Development HND course at City of Glasgow College for the subject Graded Unit 2.",
 		],
 		pic: "/img/demoScreen.png",
 	},
