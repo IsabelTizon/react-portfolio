3be6dc9413e487ed220c9cd906335c8f9cfb1908
IsabelTizon <tizonarranz@gmail.com>
2024-02-11 17:44:46 +0000

    const myProjectCopyModifily = [...myProjects]

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -11,6 +11,9 @@ export const metadata = {
 };
 
 export default function projects() {
+	console.log("myProjects", myProjects);
+	const myProjectCopyModifily = [...myProjects];
+
 	return (
 		<main className="mt-[80px]">
 			<div className="bg-gray-900  w-full p-10 md:p-42 lg:p-40 xl:p-[15rem] 2xl:p-[25rem]">
@@ -41,60 +44,65 @@ export default function projects() {
 			</p>
 
 			<div className="flex flex-col px-10 md:px-[15rem] lg:px-[25rem] xl:px-[35rem] 2xl:px-[55rem]">
-				{myProjects.reverse().map((projectsItem) => (
-					<div className="pb-[16rem]" key={projectsItem.id}>
-						<h3 className="pb-[8rem]">
-							{projectsItem.name}
-						</h3>
-						<div className="flex  flex-col md:flex-row">
-							<Image
-								className="h-auto w-full md:w-[50%] lg:w-[70%] xl:w-full object-containd rounded-t-2xl mb-10"
-								width={400}
-								height={300}
-								src={projectsItem.pic}
-								alt="project picture"
-							/>
-							<div className="md:pl-[6rem] lg:pl-[15rem] xl:pl-[15rem] 2xl:pl-[25rem]">
-								<p className="text-[3rem] line-clamp-5 mb-[2.5rem]">
-									{projectsItem.description}
-								</p>
-								<Link
-									key={projectsItem.id}
-									href={`/projects/${projectsItem.id}`}
-								>
-									<div className="flex flex-row">
-										<p className="text-[#b38f00] uppercase text-[2.5rem] mb-[6rem]">
-											read more
-										</p>
-										<MdOutlineKeyboardArrowRight
-											size={20}
-											color="#b38f00"
-											fontSize="2rem"
-										/>
-									</div>
-								</Link>
-								<div className="flex flex-row">
+				{myProjectCopyModifily
+					.reverse()
+					.map((projectsItem) => (
+						<div
+							className="pb-[16rem]"
+							key={projectsItem.id}
+						>
+							<h3 className="pb-[8rem]">
+								{projectsItem.name}
+							</h3>
+							<div className="flex  flex-col md:flex-row">
+								<Image
+									className="h-auto w-full md:w-[50%] lg:w-[70%] xl:w-full object-containd rounded-t-2xl mb-10"
+									width={400}
+									height={300}
+									src={projectsItem.pic}
+									alt="project picture"
+								/>
+								<div className="md:pl-[6rem] lg:pl-[15rem] xl:pl-[15rem] 2xl:pl-[25rem]">
+									<p className="text-[3rem] line-clamp-5 mb-[2.5rem]">
+										{projectsItem.description}
+									</p>
 									<Link
-										target="_blank"
-										href={projectsItem.url}
-									>
-										<button className="text-black tracking-widest lg:tracking-wide xl:tracking-widest uppercase mustard rounded-md mr-11">
-											visit the site
-										</button>
-									</Link>
-
-									<Link
+										key={projectsItem.id}
 										href={`/projects/${projectsItem.id}`}
 									>
-										<button className="text-black tracking-widest lg:tracking-wide xl:tracking-widest uppercase mustard p-4 rounded-md">
-											leave a comment
-										</button>
+										<div className="flex flex-row">
+											<p className="text-[#b38f00] uppercase text-[2.5rem] mb-[6rem]">
+												read more
+											</p>
+											<MdOutlineKeyboardArrowRight
+												size={20}
+												color="#b38f00"
+												fontSize="2rem"
+											/>
+										</div>
 									</Link>
+									<div className="flex flex-row">
+										<Link
+											target="_blank"
+											href={projectsItem.url}
+										>
+											<button className="text-black tracking-widest lg:tracking-wide xl:tracking-widest uppercase mustard rounded-md mr-11">
+												visit the site
+											</button>
+										</Link>
+
+										<Link
+											href={`/projects/${projectsItem.id}`}
+										>
+											<button className="text-black tracking-widest lg:tracking-wide xl:tracking-widest uppercase mustard p-4 rounded-md">
+												leave a comment
+											</button>
+										</Link>
+									</div>
 								</div>
 							</div>
 						</div>
-					</div>
-				))}
+					))}
 			</div>
 		</main>
 	);
