0767d50b962e191a142279f32be9f6d8105e9572
IsabelTizon <tizonarranz@gmail.com>
2024-02-14 11:22:06 +0000

    css footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,21 +8,43 @@ import { FaFacebook } from "react-icons/fa";
 
 const Footer = () => {
 	return (
-		<div className="flex flex-col lg:flex-row bg-slate-900">
-			<div className="flex flex-col">
-				<h5 className="text-white text-[2rem]">
+		<div className="flex flex-wrap p-[20rem] bg-slate-900">
+			{/** Isabel Tizon Column */}
+			<div className="flex flex-col px-[10rem]">
+				<h5 className="text-white text-[4rem] capitalize mb-10">
 					isabel tizón arranz
 				</h5>
-				<h6 className="text-white">junior web developer</h6>
+				<h6 className="text-white text-[3rem] capitalize mb-10">
+					junior web developer
+				</h6>
 				<div className="flex flex-row">
-					<FaFacebook size={30} color="#595959" />
-					<FaFacebook size={30} color="#595959" />
-					<FaFacebook size={30} color="#595959" />
-					<FaFacebook size={30} color="#595959" />
+					<FaFacebook
+						size={30}
+						color="#595959"
+						className="mr-10"
+					/>
+					<FaFacebook
+						size={30}
+						color="#595959"
+						className="mr-10"
+					/>
+					<FaFacebook
+						size={30}
+						color="#595959"
+						className="mr-10"
+					/>
+					<FaFacebook
+						size={30}
+						color="#595959"
+						className="mr-10"
+					/>
 				</div>
 			</div>
-			<div className="flex flex-col">
-				<h5 className="text-white">quick links</h5>
+			{/** Quick Links Column */}
+			<div className="flex flex-col px-[10rem]">
+				<h5 className="text-white text-[4rem] capitalize mb-10">
+					quick links
+				</h5>
 				<div className="flex flex-col">
 					<Link className="text-white" href="/blog">
 						<div className="flex flex-row">
@@ -31,7 +53,9 @@ const Footer = () => {
 								color="#b38f00"
 								fontSize="2rem"
 							/>
-							<h6>blog</h6>
+							<h6 className="text-[2.7rem] capitalize">
+								blog
+							</h6>
 						</div>
 					</Link>
 					<Link className="text-white" href="/projects">
@@ -41,7 +65,9 @@ const Footer = () => {
 								color="#b38f00"
 								fontSize="2rem"
 							/>
-							<h6 className="text-[2rem]">projects</h6>
+							<h6 className="text-[2.7rem] capitalize">
+								projects
+							</h6>
 						</div>
 					</Link>
 					<Link className="text-white" href="/contact">
@@ -51,7 +77,9 @@ const Footer = () => {
 								color="#b38f00"
 								fontSize="2rem"
 							/>
-							<h6>contact</h6>
+							<h6 className="text-[2.7rem] capitalize">
+								contact
+							</h6>
 						</div>
 					</Link>
 					<Link
@@ -65,59 +93,65 @@ const Footer = () => {
 								color="#b38f00"
 								fontSize="2rem"
 							/>
-							<h6>Github</h6>
+							<h6 className="text-[2.7rem] capitalize">
+								Github
+							</h6>
 						</div>
 					</Link>
 				</div>
 			</div>
-			<div className="flex flex-col">
-				<h5 className="text-white">Contact</h5>
+			{/** Contact Column */}
+			<div className="flex flex-col px-[10rem]">
+				<h5 className="text-white text-[4rem] mb-10">
+					Contact
+				</h5>
 				<div className="flex flex-col">
 					<div className="flex flex-col">
 						<div className="flex flex-row">
-							<FaFacebook size={15} color="#595959" />
-							<h6 className="text-white text-[1.5rem]">
+							<FaFacebook size={17} color="#595959" />
+							<h6 className="text-white text-[2.7rem] capitalize ml-5 mb-5">
 								Linkedin
 							</h6>
 						</div>
 						<div className="flex flex-row">
-							<FaFacebook size={15} color="#595959" />
-							<h6 className="text-white text-[1.5rem]">
+							<FaFacebook size={17} color="#595959" />
+							<h6 className="text-white text-[2.7rem] ml-5 mb-5">
 								+44 7759686358
 							</h6>
 						</div>
 						<div className="flex flex-row">
-							<FaFacebook size={15} color="#595959" />
-							<h6 className="text-white text-[1.5rem]">
+							<FaFacebook size={17} color="#595959" />
+							<h6 className="text-white text-[2.7rem] ml-5">
 								tizonarranz@gmail.com
 							</h6>
 						</div>
 					</div>
 				</div>
 			</div>
-			<div className="flex flex-col">
-				<h5 className="text-white">recent works</h5>
+			{/** Recent works Column */}
+			<div className="flex flex-col px-[10rem]">
+				<h5 className="text-white text-[4rem] capitalize mb-10">
+					recent works
+				</h5>
 
-				<div className="">
-					<div className="flex flex-wrap gap-7">
-						{/* */}
+				<div className="gap-7">
+					{/* */}
 
-						{myProjects.slice(-6).map((projectsItem) => (
-							<Link
-								key={projectsItem.id}
-								href={`/projects/${projectsItem.id}`}
-								className="w-full md:w-[32%] lg:w-[32%] xl:w-[32%] 2xl:w-[32%] bg-gray-900"
-							>
-								<Image
-									className="h-100 w-full object-containd"
-									width={80}
-									height={80}
-									src={projectsItem.pic}
-									alt="project picture"
-								/>
-							</Link>
-						))}
-					</div>
+					{myProjects.slice(-6).map((projectsItem) => (
+						<Link
+							key={projectsItem.id}
+							href={`/projects/${projectsItem.id}`}
+							className="bg-gray-900"
+						>
+							<Image
+								className="h-auto w-100% object-containd"
+								width={80}
+								height={80}
+								src={projectsItem.pic}
+								alt="project picture"
+							/>
+						</Link>
+					))}
 				</div>
 			</div>
 		</div>
