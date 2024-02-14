2f9911c64646b984b62fdd2c541d5ecfc386f6c9
IsabelTizon <tizonarranz@gmail.com>
2024-02-14 11:39:07 +0000

    spelling

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-import Footer from "@/components/footer";
+import Footer from "@/components/Footer";
 // import { Inter } from "next/font/google";
 
 // const inter = Inter({ subsets: ["latin"] });
