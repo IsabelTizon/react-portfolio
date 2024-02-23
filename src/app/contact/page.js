831a89f38257c450fd46a3617e0873b5d418904d
IsabelTizon <tizonarranz@gmail.com>
2024-02-23 13:32:02 +0000

    contact page css

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -2,7 +2,7 @@ import React from "react";
 import Link from "next/link";
 
 import { FaFacebook } from "react-icons/fa";
-import { ContactForm } from "../../components/contactForm";
+import { ContactForm } from "../../components/ContactForm";
 
 export const metadata = {
 	title: "contact",
@@ -12,18 +12,23 @@ export const metadata = {
 const contact = () => {
 	return (
 		<main className="mt-[80px]">
-			<div className="bg-gray-900  w-full p-10 md:p-42 lg:p-40 xl:p-[15rem] 2xl:p-[25rem]">
+			{/* Hero */}
+			<div className="bg-gray-900 w-full p-10 md:p-42 lg:p-40 xl:p-[15rem] 2xl:p-[25rem]">
 				<div className="flex justify-center">
 					<h1 className="yellow-letters capitalize">
 						contact me
 					</h1>
 				</div>
 			</div>
-			<div className="flex flex-col lg:flex-row">
-				<div className="flex flex-col">
+
+			<div className="flex flex-col lg:flex-row p-[10rem]">
+				{/* Get in touch Section */}
+				<div className="flex flex-col mr-[10rem] ml-[7rem]  w-full lg:w-2/5">
 					<div>
-						<h3 className="capitalize">get in touch</h3>
-						<p>
+						<h3 className="capitalize mb0-[12rem]">
+							get in touch
+						</h3>
+						<p className="pb-[10rem]">
 							If you liked my portfolio and are interested
 							in working with me, you can contact me in the
 							following ways.
@@ -57,18 +62,16 @@ const contact = () => {
 						</div>
 					</div>
 				</div>
-				<div>
-					{" "}
-					<h3 className="capitalize">form</h3>
-					<p>
-						If you liked my portfolio and are interested in
-						working with me, you can contact me in the
-						following ways.
-					</p>
-					<ContactForm />
+				{/* Form Section */}
+				<div className=" w-full lg:w-3/5">
+					<div className="p-[5rem] rounded-3xl bg-[#fff8e2]">
+						<ContactForm />
+					</div>
 				</div>
 			</div>
-			<div className="bg-[#bdddf8]  w-full p-10 md:p-42 lg:p-40 xl:p-[15rem] 2xl:p-[25rem]">
+
+			{/* API*/}
+			<div className="bg-[#bdddf8] w-full p-10 md:p-42 lg:p-40 xl:p-[15rem] 2xl:p-[25rem]">
 				<div className="flex justify-center">
 					<h1 className="yellow-letters capitalize">API</h1>
 				</div>
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,8 +2,9 @@ import React from "react";
 
 export const ContactForm = () => {
 	return (
-		<form>
-			<div>
+		<form className="mr-[7rem]">
+			{/* Name */}
+			<div className="flex flex-col">
 				<label className="label-form" htmlFor="name">
 					Name
 				</label>
@@ -11,6 +12,29 @@ export const ContactForm = () => {
 					id="name"
 					type="text"
 					name="name"
+					className="input-form"
+				/>
+			</div>
+			{/* Email */}
+			<div className="flex flex-col">
+				<label className="label-form" htmlFor="email">
+					Email
+				</label>
+				<imput
+					id="email"
+					type="email"
+					name="email"
+					className="input-form"
+				/>
+			</div>
+			{/* Comment */}
+			<div className="">
+				<label className="label-form" htmlFor="comment">
+					Comment
+				</label>
+				<textarea
+					id="comment"
+					name="comment"
 					className="input-form"
 				/>
 			</div>
