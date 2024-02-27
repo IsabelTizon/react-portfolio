170bbe806237ca410d4eaaaad9e6e1c8dd21c69d
IsabelTizon <tizonarranz@gmail.com>
2024-02-27 16:41:36 +0000

    CSS mistake Navbar fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ function Navbar() {
 	};
 	return (
 		<header className="bg-slate-400 text-black w-full ease-in duration-300 fixed top-0 left-0 z-10">
-			<nav className="max-w-[1366px] mx-auto h-[80px] flex justify-between items-center  p-4">
+			<nav className="w-full h-[80px] flex justify-between items-center  p-4">
 				<div>
 					<Link
 						className="uppercase font-extrabold text-[4rem] md:text-[4.5rem] lg:text-[6rem] text-slate-800 hover:text-[#381638]"
