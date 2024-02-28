b9e24d91b2b59aba63757175e0e55dcbd6026a0e
IsabelTizon <tizonarranz@gmail.com>
2024-02-28 18:17:12 +0000

    css ContactForm Component

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -41,6 +41,9 @@ export const ContactForm = () => {
 					className="input-form h-[30px] text-[3rem]"
 					value={formData.name}
 					onChange={handleChange}
+					required
+					minLength={3}
+					maxLength={30}
 				/>
 			</div>
 			{/* Email */}
@@ -55,9 +58,12 @@ export const ContactForm = () => {
 					id="email"
 					type="email"
 					name="email"
-					className="input-form h-[30px]"
+					className="input-form h-[30px] text-[3rem]"
 					value={formData.email}
 					onChange={handleChange}
+					required
+					minLength={3}
+					maxLength={30}
 				/>
 			</div>
 			{/* Message */}
@@ -71,9 +77,12 @@ export const ContactForm = () => {
 				<textarea
 					id="message"
 					name="message"
-					className="input-form h-[200px]"
+					className="input-form h-[200px] text-[2.8rem]"
 					value={formData.message}
 					onChange={handleChange}
+					required
+					minLength={10}
+					maxLength={500}
 				/>
 			</div>
 			<button className="mustard mt-[35px]" type="submit">
