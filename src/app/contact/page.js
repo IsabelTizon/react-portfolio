915dec54b3efab5f6ad8ac4ab6dccbaf822f93b8
Isabel <tizonarranz@gmail.com>
2025-02-19 19:48:22 +0100

    css updated

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -18,71 +18,83 @@ const contact = () => {
 	return (
 		<main className="mt-[80px]">
 			{/* Hero */}
-			<div className="bg-gray-900 w-full p-10">
+			<div className="bg-[#111827] grid-bg w-full p-10">
 				<div className="flex justify-center">
-					<h1 className="yellow-letters capitalize">
-						contact
-					</h1>
+					<h1 className="text-light capitalize">contact</h1>
 				</div>
 			</div>
 			{/* Get in contact (Links & Form) */}
-			<div className="w-90% m-10 xl:w-80% xl:mx-[10%] xl:my-[100px] 2xl:w-[70%] 2xl:my-[160px] 2xl:mx-[15%]">
-				<div className="flex flex-col lg:flex-row">
-					{/* Get in touch Section */}
-					<div className="flex flex-col w-full lg:w-1/2 lg:pr-[15rem]">
-						<div>
-							<h3 className="capitalize text-[#9966ff] font-bold">
-								get in touch
-							</h3>
-							<p className="">
-								If you liked my portfolio and are interested
-								in working with me, you can contact me in
-								the following ways.
-							</p>
+			<div className="bg-[#111827]">
+				<div className="">
+					<div className="flex flex-col lg:flex-row">
+						{/* Get in touch Section */}
+						<div className="flex flex-col w-full lg:w-1/2">
+							<div>
+								<h3 className="capitalize text-light font-bold">
+									get in touch
+								</h3>
+								<p className="text-light">
+									If you liked my portfolio and are
+									interested in working with me, feel free
+									to contact me through the following
+									channels.
+								</p>
+							</div>
+							{/* Linkedin & Github */}
+							<div className="flex flex-row">
+								<div className="flex flex-row w-1/2">
+									<BsLinkedin size={30} color="#9966ff" />
+									<p className="text-light flex items-center ml-8">
+										Linkedin
+									</p>
+								</div>
+								<div className="flex flex-row w-1/2">
+									<FaSquareGithub
+										size={30}
+										color="#9966ff"
+									/>
+									<p className="text-light flex items-center ml-8">
+										GitHub
+									</p>
+								</div>
+							</div>
+							{/* My phone & My email */}
+							<div className="flex flex-col">
+								<div className="flex flex-row">
+									<FaPhoneSquareAlt
+										size={30}
+										color="#9966ff"
+									/>
+									<p className="text-light flex items-center ml-8">
+										+44 7759686358
+									</p>
+								</div>
+								<div className="flex flex-row">
+									<FaPhoneSquareAlt
+										size={30}
+										color="#9966ff"
+									/>
+									<p className="text-light flex items-center ml-8">
+										+34 675474558
+									</p>
+								</div>
+								<div className="flex flex-row">
+									<MdEmail size={30} color="#9966ff" />
+									<p className="text-light flex items-center ml-8">
+										tizonarranz@gmail.com
+									</p>
+								</div>
+							</div>
 						</div>
-						{/* Linkedin & Github */}
-						<div className="flex flex-row">
-							<div className="flex flex-row w-1/2">
-								<BsLinkedin size={60} color="#b38f00" />
-								<p className="flex items-center ml-8">
-									Linkedin
-								</p>
+						{/* Form Section */}
+						<div className=" w-full lg:w-1/2">
+							<div className="border-1 border-solid border-[#19160f]">
+								<ContactForm />
 							</div>
-							<div className="flex flex-row w-1/2">
-								<FaSquareGithub size={60} color="#b38f00" />
-								<p className="flex items-center ml-8">
-									GitHub
-								</p>
-							</div>
-						</div>
-						{/* My phone & My email */}
-						<div className="flex flex-row mb-[80px]">
-							<div className="flex flex-row w-1/2">
-								<FaPhoneSquareAlt
-									size={34}
-									color="#b38f00"
-								/>
-								<p className="flex items-center ml-8">
-									+44 7759686358
-								</p>
-							</div>
-							<div className="flex flex-row w-1/2">
-								<MdEmail size={34} color="#b38f00" />
-								<p className="flex items-center ml-8">
-									tizonarranz@gmail.com
-								</p>
-							</div>
-						</div>
-					</div>
-					{/* Form Section */}
-					<div className=" w-full lg:w-1/2">
-						<div className="rounded-3xl border-1 border-solid border-[#19160f]">
-							<ContactForm />
 						</div>
 					</div>
 				</div>
 			</div>
-
 			{/* API Map */}
 			<div className="bg-[#bdddf8] w-full">
 				<div className="flex justify-center">
--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -16,85 +16,87 @@ export default function projects() {
 
 	return (
 		<main className="mt-[80px]">
-			<div className="bg-gray-900 w-full p-10">
+			<div className="bg-[#111827] grid-bg w-full p-10">
 				<div className="flex justify-center">
-					<h1 className="yellow-letters capitalize">
+					<h1 className="text-light capitalize">
 						my projects
 					</h1>
 				</div>
 			</div>
-			<div className="flex justify-center">
-				<h2 className="capitalize p-10">
-					You&apos;re welcome to take a look!
-				</h2>
-			</div>
-			<div className="flex justify-center">
-				<p className="pb-40 px-10">
-					This portfolio showcases my learning process
-					throughout my career. You can see my progress in
-					descending order, with the most recent project
-					displayed first, followed by all the others. The
-					oldest chronologically date from my stay in the
-					Web development course (HNC&HND) at the City of
-					Glasgow College.
-				</p>
-			</div>
-			<div className="flex flex-col px-10">
-				{myProjectCopyModifily
-					.reverse()
-					.map((projectsItem) => (
-						<div className="" key={projectsItem.id}>
-							<h3 className="">{projectsItem.name}</h3>
-							<div className="flex  flex-col md:flex-row">
-								<Image
-									className="h-auto w-full"
-									width={400}
-									height={50}
-									src={projectsItem.pic}
-									alt="project picture"
-								/>
-								<div className="">
-									<p className="line-clamp-5 mb-[2.5rem]">
-										{projectsItem.description}
-									</p>
-									<Link
-										key={projectsItem.id}
-										href={`/projects/${projectsItem.id}`}
-									>
+			<div className="bg-[#111827]">
+				<div className="flex justify-center">
+					<h2 className="text-light capitalize p-10">
+						You&apos;re welcome to take a look!
+					</h2>
+				</div>
+				<div className="flex justify-center">
+					<p className="text-light">
+						This portfolio showcases my learning process
+						throughout my career. You can see my progress in
+						descending order, with the most recent project
+						displayed first, followed by all the others. The
+						oldest chronologically date from my stay in the
+						Web development course (HNC&HND) at the City of
+						Glasgow College.
+					</p>
+				</div>
+				<div className="flex flex-col px-10">
+					{myProjectCopyModifily
+						.reverse()
+						.map((projectsItem) => (
+							<div className="" key={projectsItem.id}>
+								<h3 className="">{projectsItem.name}</h3>
+								<div className="flex flex-col md:flex-row">
+									<Image
+										className="h-auto w-full"
+										width={400}
+										height={50}
+										src={projectsItem.pic}
+										alt="project picture"
+									/>
+									<div className="">
+										<p className="text-light line-clamp-5 mb-[2.5rem]">
+											{projectsItem.description}
+										</p>
+										<Link
+											key={projectsItem.id}
+											href={`/projects/${projectsItem.id}`}
+										>
+											<div className="flex flex-row">
+												<p className="primary-500 uppercase mb-[6rem] hover:text-[#000000]">
+													read more
+												</p>
+												<MdOutlineKeyboardArrowRight
+													size={20}
+													color="primary-500"
+													fontSize="1rem"
+												/>
+											</div>
+										</Link>
 										<div className="flex flex-row">
-											<p className="primary-500 uppercase mb-[6rem] hover:text-[#000000]">
-												read more
-											</p>
-											<MdOutlineKeyboardArrowRight
-												size={20}
-												color="primary-500"
-												fontSize="2rem"
-											/>
-										</div>
-									</Link>
-									<div className="flex flex-row">
-										<Link
-											target="_blank"
-											href={projectsItem.url}
-										>
-											<button className="purple">
-												visit the site
-											</button>
-										</Link>
+											<Link
+												target="_blank"
+												href={projectsItem.url}
+											>
+												<button className="purple">
+													visit the site
+												</button>
+											</Link>
 
-										<Link
-											target="_blank"
-											href={projectsItem.github}
-										>
-											<button className="purple-full">
-												GitHub
-											</button>
-										</Link>
+											<Link
+												target="_blank"
+												href={projectsItem.github}
+											>
+												<button className="purple-full">
+													GitHub
+												</button>
+											</Link>
+										</div>
 									</div>
 								</div>
 							</div>
-						</div>
-					))}
+						))}
+				</div>
 			</div>
 		</main>
 	);
