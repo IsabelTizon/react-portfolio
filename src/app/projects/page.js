650e13af4e011064f33784f7b2dd024e156f7670
Isabel <tizonarranz@gmail.com>
2025-02-28 17:47:00 +0100

    Updated CSS for ProjectCard component

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,6 +1,6 @@
 import { myProjects } from "../data/projects";
 import Hero from "../../components/global/Hero";
-import ProjectCard from "../../components/projects/projectCard";
+import ProjectCard from "../../components/projects/ProjectCard";
 
 export const metadata = {
 	title: "Projects page",
@@ -16,31 +16,33 @@ export default function projects() {
 			<Hero title="My Projects" />
 
 			<div className="bg-[#111827]">
-				<div className="flex justify-center">
-					<h2 className="text-light capitalize p-10">
-						You&apos;re welcome to take a look!
-					</h2>
-				</div>
-				<div className="flex justify-center">
-					<p className="text-light">
-						This portfolio showcases my learning process
-						throughout my career. You can see my progress in
-						descending order, with the most recent project
-						displayed first, followed by all the others. The
-						oldest chronologically date from my stay in the
-						Web development course (HNC&HND) at the City of
-						Glasgow College.
-					</p>
-				</div>
-				<div className="flex flex-col px-10">
-					{myProjectCopyModifily
-						.reverse()
-						.map((projectsItem) => (
-							<ProjectCard
-								key={projectsItem.id}
-								project={projectsItem}
-							/>
-						))}
+				<div className=" w-[80%] m-auto">
+					<div className="flex justify-center">
+						<h2 className="text-light capitalize p-10">
+							You&apos;re welcome to take a look!
+						</h2>
+					</div>
+					<div className="flex justify-center">
+						<p className="text-light">
+							This portfolio showcases my learning process
+							throughout my career. You can see my progress
+							in descending order, with the most recent
+							project displayed first, followed by all the
+							others. The oldest chronologically date from
+							my stay in the Web development course
+							(HNC&HND) at the City of Glasgow College.
+						</p>
+					</div>
+					<div className="flex flex-col">
+						{myProjectCopyModifily
+							.reverse()
+							.map((projectsItem) => (
+								<ProjectCard
+									key={projectsItem.id}
+									project={projectsItem}
+								/>
+							))}
+					</div>
 				</div>
 			</div>
 		</main>
--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -1,30 +1,34 @@
-// components/ProjectCard.js
 import Link from "next/link";
 import Image from "next/image";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
 const ProjectCard = ({ project }) => {
 	return (
-		<div key={project.id}>
-			<h3 className="text-light">{project.name}</h3>
-			<div className="flex flex-col md:flex-row">
-				<Image
-					className="h-auto w-full"
-					width={400}
-					height={50}
-					src={project.pic}
-					alt="project picture"
-				/>
-				<div className="">
-					<p className="text-light line-clamp-5 mb-[2.5rem]">
+		<div className="my-11" key={project.id}>
+			<h3 className="text-light mb-6">{project.name}</h3>
+			<div className="flex flex-col md:flex-row gap-6 items-center">
+				{/* Imagen (Izquierda) */}
+				<div className="w-full md:w-1/2">
+					<Image
+						className="rounded-lg"
+						width={500}
+						height={300}
+						src={project.pic}
+						alt="project picture"
+					/>
+				</div>
+				{/* Contenido (Derecha) */}
+				<div className="w-full md:w-1/2 flex flex-col">
+					<p className="text-light line-clamp-5 mb-[1rem]">
 						{project.description}
 					</p>
+					{/* Enlace "Read More" */}
 					<Link
 						key={project.id}
 						href={`/projects/${project.id}`}
 					>
 						<div className="flex flex-row">
-							<p className="primary-500 uppercase mb-[6rem] hover:text-[#000000]">
+							<p className="primary-500 uppercase mb-[2rem] hover:text-[#000000]">
 								read more
 							</p>
 							<MdOutlineKeyboardArrowRight
@@ -34,13 +38,13 @@ const ProjectCard = ({ project }) => {
 							/>
 						</div>
 					</Link>
-					<div className="flex flex-row">
+					{/* Botones */}
+					<div className="flex flex-row gap-8">
 						<Link target="_blank" href={project.url}>
 							<button className="purple">
 								visit the site
 							</button>
 						</Link>
-
 						<Link target="_blank" href={project.github}>
 							<button className="purple-full">
 								GitHub
