aa439ed603a7ce9df0c2549f58a5a1c52d10a308
Isabel <tizonarranz@gmail.com>
2025-03-01 14:02:39 +0100

    ContactDetails Component created

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,14 +1,6 @@
-import React from "react";
-import Link from "next/link";
 import Hero from "../../components/global/Hero";
-
-import { BsLinkedin } from "react-icons/bs";
-import { FaPhoneSquareAlt } from "react-icons/fa";
-import { MdEmail } from "react-icons/md";
-
-import { FaSquareGithub } from "react-icons/fa6";
-
-import { ContactForm } from "../../components/global/ContactForm";
+import { ContactDetails } from "@/components/contact/ContactDetails";
+import { ContactForm } from "../../components/contact/ContactForm";
 
 export const metadata = {
 	title: "contact",
@@ -23,76 +15,12 @@ const contact = () => {
 
 			{/* Get in contact (Links & Form) */}
 			<div className="bg-[#111827]">
-				<div className="">
-					<div className="flex flex-col lg:flex-row">
-						{/* Get in touch Section */}
-						<div className="flex flex-col w-full lg:w-1/2">
-							<div>
-								<h3 className="capitalize text-light font-bold">
-									get in touch
-								</h3>
-								<p className="text-light">
-									If you liked my portfolio and are
-									interested in working with me, feel free
-									to contact me through the following
-									channels.
-								</p>
-							</div>
-							{/* Linkedin & Github */}
-							<div className="flex flex-row">
-								<div className="flex flex-row w-1/2">
-									<BsLinkedin size={30} color="#9966ff" />
-									<p className="text-light flex items-center ml-8">
-										Linkedin
-									</p>
-								</div>
-								<div className="flex flex-row w-1/2">
-									<FaSquareGithub
-										size={30}
-										color="#9966ff"
-									/>
-									<p className="text-light flex items-center ml-8">
-										GitHub
-									</p>
-								</div>
-							</div>
-							{/* My phone & My email */}
-							<div className="flex flex-col">
-								<div className="flex flex-row">
-									<FaPhoneSquareAlt
-										size={30}
-										color="#9966ff"
-									/>
-									<p className="text-light flex items-center ml-8">
-										+44 7759686358
-									</p>
-								</div>
-								<div className="flex flex-row">
-									<FaPhoneSquareAlt
-										size={30}
-										color="#9966ff"
-									/>
-									<p className="text-light flex items-center ml-8">
-										+34 675474558
-									</p>
-								</div>
-								<div className="flex flex-row">
-									<MdEmail size={30} color="#9966ff" />
-									<p className="text-light flex items-center ml-8">
-										tizonarranz@gmail.com
-									</p>
-								</div>
-							</div>
-						</div>
-						{/* Form Section */}
-						<div className=" w-full lg:w-1/2">
-							<div className="border-1 border-solid border-[#19160f]">
-								<ContactForm />
-							</div>
-						</div>
-					</div>
+				<div className="flex flex-col lg:flex-row">
+					<ContactDetails />
+					<ContactForm />
 				</div>
 			</div>
+
 			{/* API Map */}
 			<div className="bg-[#bdddf8] w-full">
 				<div className="flex justify-center">
---
+++ src/components/contact/ContactDetails.js
@@ -0,0 +1,74 @@
+import Link from "next/link";
+
+{
+	/* icons */
+}
+import { BsLinkedin } from "react-icons/bs";
+import { FaPhoneSquareAlt } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { FaSquareGithub } from "react-icons/fa6";
+
+export const ContactDetails = () => {
+	return (
+		<div className="flex flex-col">
+			<div>
+				<h3 className="capitalize text-light font-bold">
+					get in touch
+				</h3>
+				<p className="text-light">
+					If you liked my portfolio and are interested in
+					working with me, feel free to reach out to me
+					through the following channels
+				</p>
+			</div>
+
+			<div className="flex flex-col">
+				{/* Linkedin & Github */}
+				<div className="flex flex-row">
+					<div className="flex-1">
+						<div className="flex flex-row">
+							<BsLinkedin size={30} color="#9966ff" />
+							<p className="text-light flex items-center ml-2">
+								Linkedin
+							</p>
+						</div>
+					</div>
+					<div className="flex-1">
+						<div className="flex flex-row">
+							<FaSquareGithub size={30} color="#9966ff" />
+							<p className="text-light flex items-center ml-2">
+								GitHub
+							</p>
+						</div>
+					</div>
+				</div>
+				{/* phones*/}
+				<div className="flex flex-row">
+					<div className="flex-1">
+						<div className="flex flex-row">
+							<FaPhoneSquareAlt size={30} color="#9966ff" />
+							<p className="text-light flex items-center ml-2">
+								+44 7759686358
+							</p>
+						</div>
+					</div>
+					<div className="flex-1">
+						<div className="flex flex-row">
+							<FaPhoneSquareAlt size={30} color="#9966ff" />
+							<p className="text-light flex items-center ml-2">
+								+34 675474558
+							</p>
+						</div>
+					</div>
+				</div>
+				{/* email */}
+				<div className="flex flex-row ">
+					<MdEmail size={30} color="#9966ff" />
+					<p className="text-light flex items-center ml-2">
+						tizonarranz@gmail.com
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+};
