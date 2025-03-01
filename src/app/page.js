7a2912ebc60057fede8e69013ddb9c4ff72a68cb
Isabel <tizonarranz@gmail.com>
2025-03-01 10:14:43 +0100

    skills component created

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import Skills from "@/components/home/skills";
 import {
 	languages,
 	frameworks,
@@ -5,7 +6,6 @@ import {
 	concepts,
 } from "./data/skills";
 import { myProjects } from "./data/projects";
-
 import Image from "next/image";
 import Link from "next/link";
 
@@ -40,89 +40,9 @@ export default function About() {
 				</Link>
 			</div>
 			{/* My skills */}
-			<div className="bg-[#303c55] grid-bg">
-				<div className="py-[5%] px-[10%]">
-					<div className="flex justify-center">
-						<h2 className="text-light underline underline-offset-8 py-9">
-							Technical skills
-						</h2>
-					</div>
-					<div className="flex flex-wrap">
-						{/* first column */}
-						<div className="w-full md:w-1/2 lg:w-1/4">
-							<h4 className="primary-400 font-thin tracking-wide uppercase pb-8">
-								Languages
-							</h4>
-							<div className="">
-								<ul>
-									{languages.map((language) => (
-										<li
-											key={language.id}
-											className="text-light"
-										>
-											{language.name}
-										</li>
-									))}
-								</ul>
-							</div>
-						</div>
-						{/* second column */}
-						<div className="w-full md:w-1/2 lg:w-1/4">
-							<h4 className="primary-400 font-thin tracking-wide uppercase pb-8">
-								Libraries and Frameworks
-							</h4>
-							<div className="">
-								<ul>
-									{frameworks.map((framework) => (
-										<li
-											key={framework.id}
-											className="text-light"
-										>
-											{framework.name}
-										</li>
-									))}
-								</ul>
-							</div>
-						</div>
-						{/* third column */}
-						<div className="w-full md:w-1/2 lg:w-1/4">
-							<h4 className="primary-400 font-thin tracking-wide uppercase pb-8">
-								Tools
-							</h4>
-							<div className="">
-								<ul>
-									{tools.map((tool) => (
-										<li
-											key={tool.id}
-											className="text-light"
-										>
-											{tool.name}
-										</li>
-									))}
-								</ul>
-							</div>
-						</div>
-						{/* fourth column */}
-						<div className="w-full md:w-1/2 lg:w-1/4">
-							<h4 className="primary-400 font-thin tracking-wide uppercase pb-8">
-								Concepts
-							</h4>
-							<div className="">
-								<ul>
-									{concepts.map((concept) => (
-										<li
-											key={concept.id}
-											className="text-light"
-										>
-											{concept.name}
-										</li>
-									))}
-								</ul>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
+			<Skills
+				skills={{ languages, frameworks, tools, concepts }}
+			/>
 			{/* About me */}
 			<div className="bg-[#111827]">
 				<div className="w-[95%] m-auto">
---
+++ src/components/home/Skills.js
@@ -0,0 +1,90 @@
+const Skills = ({ skills }) => {
+	return (
+		<div>
+			<div className="bg-[#303c55] grid-bg">
+				<div className="py-[5%] px-[10%]">
+					<div className="flex justify-center">
+						<h2 className="text-light underline underline-offset-8 py-9">
+							Technical skills
+						</h2>
+					</div>
+					<div className="flex flex-wrap">
+						{/* first column */}
+						<div className="w-full md:w-1/2 lg:w-1/4">
+							<h4 className="primary-400 font-thin tracking-wide uppercase pb-8">
+								Languages
+							</h4>
+							<div className="">
+								<ul>
+									{skills.languages.map((language) => (
+										<li
+											key={language.id}
+											className="text-light"
+										>
+											{language.name}
+										</li>
+									))}
+								</ul>
+							</div>
+						</div>
+						{/* second column */}
+						<div className="w-full md:w-1/2 lg:w-1/4">
+							<h4 className="primary-400 font-thin tracking-wide uppercase pb-8">
+								Libraries and Frameworks
+							</h4>
+							<div className="">
+								<ul>
+									{skills.frameworks.map((framework) => (
+										<li
+											key={framework.id}
+											className="text-light"
+										>
+											{framework.name}
+										</li>
+									))}
+								</ul>
+							</div>
+						</div>
+						{/* third column */}
+						<div className="w-full md:w-1/2 lg:w-1/4">
+							<h4 className="primary-400 font-thin tracking-wide uppercase pb-8">
+								Tools
+							</h4>
+							<div className="">
+								<ul>
+									{skills.tools.map((tool) => (
+										<li
+											key={tool.id}
+											className="text-light"
+										>
+											{tool.name}
+										</li>
+									))}
+								</ul>
+							</div>
+						</div>
+						{/* fourth column */}
+						<div className="w-full md:w-1/2 lg:w-1/4">
+							<h4 className="primary-400 font-thin tracking-wide uppercase pb-8">
+								Concepts
+							</h4>
+							<div className="">
+								<ul>
+									{skills.concepts.map((concept) => (
+										<li
+											key={concept.id}
+											className="text-light"
+										>
+											{concept.name}
+										</li>
+									))}
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+export default Skills;
