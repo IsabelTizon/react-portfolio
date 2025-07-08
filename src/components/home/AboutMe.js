ff0278f1aeeb9e5708bb4462ffd912885b12ea74
Isabel <tizonarranz@gmail.com>
2025-07-08 10:14:06 +0200

    About Me description updated

--- src/components/home/AboutMe.js
+++ src/components/home/AboutMe.js
@@ -6,29 +6,23 @@ export const AboutMe = () => {
 			<div className="w-[95%] lg:w-[80%] m-auto">
 				<div className="flex flex-wrap">
 					<div className="w-full md:w-1/2">
-						<h2 className="secundary-300 mb-2">About me</h2>
+						<h2 className="secundary-300 mb-6">About me</h2>
 
 						<p className="pb-4 text-light">
-							I am a Web Development student in my second
-							year, with graduation in June 2023. During
-							these two years at the City of Glasgow
-							College, as well as also self-studying in my
-							free time, I have learned a lot about web
-							development.
+							Passionate about web development and currently
+							working on a project using React and NestJS.
+							I’m currently studying a Master in Advanced
+							Web Development and previously completed an
+							HND in Web Development in Glasgow.
 						</p>
 						<p className="pb-4 text-light">
-							For this reason, my intention now is to find
-							an internship and use my knowledge and skills
-							I have developed.
-						</p>
-						<p className="pb-4 text-light">
-							I consider myself to be a very hard-working
-							person, a great team player and I always try
-							to work on my own initiative to overcome any
-							obstacles.
-						</p>
-						<p className="pb-4 text-light">
-							Fluency in both English and Spanish.
+							I’m excited to join a collaborative team that
+							follows agile methodologies and values best
+							practices like Clean Code. With strong
+							communication skills in English and a
+							friendly, positive attitude, I prioritize both
+							the quality of my work and building strong
+							relationships.
 						</p>
 					</div>
 
